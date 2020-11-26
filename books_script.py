db998b041b2f0bb896b7ad80848686316f37b168
Joshua Owens <jowens@palantir.com>
2020-11-25 21:00:27 -0500

    argparsed

--- books_script.py
+++ books_script.py
@@ -10,25 +10,25 @@ import argparse
 
 # CONSTANTS
 API_URL = 'https://www.googleapis.com/books/v1/volumes'
+SECRET  = 'AIzaSyBKFOVhps_PAJaA5mq9n440F_ILdj8BCMM'
+_SEARCH_ARGVAR = 'search_term'
 
-# vars
-parameters = dict(q='')
-startIndex = 0
-search = ''  
-results = '' 
+# result vars
+results = {}
 responseTime = 0
 maxDate = None
 minDate = None
 mostProlific = ''
 
-_STERM_ARGVAR = 'search_term'
  
 def _parse_args(argv):
     ''' Parses args into a dict of ARGVAR=value, or None if the argument wasn't supplied '''
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # TODO Add arguments here, e.g.:
-    # parser.add_argument(_SAMPLE_ARGVAR, metavar="<sample>", help="This variable is a sample variable")
-    parser.add_argument(_STERM_ARGVAR, metavar="<search>", help="Your search term(s)"
+    parser.add_argument(_SEARCH_ARGVAR, metavar="<search>", help="Your search term(s)")
+    parser.add_argument('-t', '--intitle', help="filter books by keywords contained in the title")
+    parser.add_argument('-a', '--inauthor', help="filter books by keywords contained in the author")
+    parser.add_argument('-p', '--inpublisher', help="filter books by keywords contained in the publisher")
+    parser.add_argument('-s', '--subject', help="filter books by keywords in theb book subject or category list")
     return vars(parser.parse_args(argv))
  
 def _print_error(msg):
@@ -36,9 +36,14 @@ def _print_error(msg):
  
 def _validate_args(args):
     ''' Performs validation on the given args dict, returning a non-zero exit code if errors were found or None if all is well '''
-    if args[_STERM_ARGVAR] is None:
+    if args[_SEARCH_ARGVAR] is None:
         return "You must supply a search term."
     return None
+
+def format_request(query, start, maxRes):
+    '''compiles a request from a few simple components'''
+    
+    
  
 def main(argv):
     args = _parse_args(map(str, argv))
@@ -46,8 +51,22 @@ def main(argv):
     if err is not None:
         return err
  
+    # pull arg vals out of args
+    search_term = args[_SEARCH_ARGVAR]
+    intitle = args.intitle
+    inauthor = args.inauthor
+    inpublisher = args.inpublisher
+    subject = args.subject
+    query_args = [search_term, intitle, inauthor, inpublisher, subject]
+
+    # establish a starting point
+    startIndex = 0
+    maxResults = 40
+
+
+
     
- 
+
     return 0
  
 if __name__ == "__main__":
